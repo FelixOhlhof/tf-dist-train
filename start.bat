4520920945151dc9acdb6da75aef044974086123
Felix <foh@weko.de>
2022-06-08 09:40:09 +0200

    batch reads total clients from ini file and starts them

--- start.bat
+++ start.bat
@@ -1,4 +1,12 @@
-start cmd.exe /k python server.py
-start cmd.exe /k python client.py -i 1
-start cmd.exe /k python client.py -i 2
-start cmd.exe /k python client.py -i 3
+@echo off & setlocal enabledelayedexpansion
+
+for /f "tokens=1,2 delims==" %%a in (config.ini) do (
+	if "%%a"=="TOTAL_CLIENTS" (
+		set "TOTAL_CLIENTS=%%b"
+		echo TOTAL_CLIENTS: !TOTAL_CLIENTS!
+		start cmd.exe /k python server.py
+		for /l %%x in (1, 1, !TOTAL_CLIENTS!: ) do (
+			start cmd.exe /k python client.py -i %%x
+		)
+	)
+)
